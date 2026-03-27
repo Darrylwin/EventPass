798075dd4685823810b50986b22a485508d0ef66
Darrylwin <logossoudarryl20@gmail.com>
2026-03-27 00:23:05 +0000

    Implement FilamentUser interface in User model and add access control for admin panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -59,6 +60,9 @@ class User extends Authenticatable
         return $this->hasMany(Registration::class);
     }
 
+    /**
+     * Seul l'administrateur peut accéder au panel Filament.
+     */
     public function canAccessPanel(Panel $panel): bool
     {
         return $this->isAdmin();
